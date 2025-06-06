632f693a8bae041d4d05e3e72c78ca205d3a72f7
Axel Velasquez <yoloax11@gmail.com>
2025-06-06 13:46:05 -0700

    simple footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => {
+
+    return (
+        <div className="h-12 text-center flex justify-center items-center py-14">
+            <p>&copy; Innovate Recreation. All rights reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
