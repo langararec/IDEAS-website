6d580b324b873d1e5aae098667c1e75d26433590
Axel Velasquez <yoloax11@gmail.com>
2025-06-05 22:41:34 -0700

    created the carousel component

---
+++ frontend/src/pages/Home/Carousel.tsx
@@ -0,0 +1,39 @@
+import Marquee from "react-fast-marquee";
+
+const CarouselUrls = [
+    "/carousel1.webp",
+    "/carousel2.webp",
+    "/carousel3.webp",
+    "/carousel4.webp"
+];
+
+const Carousel: React.FC = () => {
+
+    return (
+        <Marquee
+            speed={30}
+            gradient={true}
+            gradientColor="#ffffff"
+            gradientWidth={20}
+            className="py-8 overflow-y-hidden"
+            pauseOnHover={true}
+            autoFill={true}
+
+        >
+            {CarouselUrls.map((url, index) => {
+                return (
+                    <div key={index} className="flex-shrink-0">
+                        <img
+                            src={url}
+                            alt={`Carousel Image ${index + 1}`}
+                            className={`h-60 xl:h-72 object-cover rounded-lg shadow-lg mx-4 ${index % 2 === 0 ? 'mt-16' : ''}`}
+                        />
+                    </div>
+                );
+            })}
+
+        </Marquee>
+    );
+}
+
+export default Carousel;
