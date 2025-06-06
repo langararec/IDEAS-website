a7f9290b54d0466a3e6afe43e675ae9b60b2d70a
Axel Velasquez <yoloax11@gmail.com>
2025-06-05 22:41:02 -0700

    modified the app to add test route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
 import Navbar from './components/Navbar'
+
+import Test from './pages/Test'
 function App() {
 
 
@@ -12,6 +14,8 @@ function App() {
       <div className='mt-24'></div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+        {/* Add more routes as needed */}
       </Routes>
     </Router>
   )
