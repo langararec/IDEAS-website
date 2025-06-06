2c16e9717dda426be3d0aa791b7826ccfec1c3aa
Axel Velasquez <yoloax11@gmail.com>
2025-06-06 13:47:20 -0700

    separated the hero component for the  homepage

---
+++ frontend/src/pages/Home/Hero.tsx
@@ -0,0 +1,19 @@
+import Carousel from "./Carousel";
+
+const Hero: React.FC = () => {
+    return (
+        <div className="px-4 py-12">
+            <div className="text-center max-w-6xl mx-auto">
+                <h1 className="text-4xl lg:text-6xl leading-tight font-bold lg:font-bold font-dm-sans text-primary mb-8">
+                    Inclusion, Diversity, Equity, and Accessibility in <span className="text-accent">Public Recreation</span>
+                </h1>
+                <p className="max-w-2xl text-lg lg:text-xl text-black mx-auto leading-relaxed mb-12 font-dm-sans">
+                    Our research initiative focused on the experiences of immigrant and racialized communities in British Columbia's public recreation.
+                </p>
+                <Carousel />
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
