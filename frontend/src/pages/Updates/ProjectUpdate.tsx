3c0264be6d7de3d2ec9938c2d3222b8b4fcdee17
Axel Velasquez <yoloax11@gmail.com>
2025-06-20 13:13:44 -0700

    created the projectupdate component

---
+++ frontend/src/pages/Updates/ProjectUpdate.tsx
@@ -0,0 +1,11 @@
+const ProjectUpdates = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-2xl font-bold mb-4 text-primary">Coming Soon! </h1>
+
+    </div>
+  );
+}
+
+
+export default ProjectUpdates;
