44b0f1191aa09a7cc844a60a2dc6e03d59d01ed0
Axel Velasquez <yoloax11@gmail.com>
2025-06-27 20:11:37 -0700

    added the formspree provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { FormspreeProvider } from '@formspree/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FormspreeProvider project="2769235073248001641">
+      <App />
+    </FormspreeProvider>
   </StrictMode>,
 )
