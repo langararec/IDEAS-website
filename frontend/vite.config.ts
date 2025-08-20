cf3c0306736d98925564ea3660e27c2eb10c6eb9
Axel Velasquez <yoloax11@gmail.com>
2025-08-20 15:12:42 -0700

    added the image optimizer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), ViteImageOptimizer({
+            png: { quality: 90 },
+            jpeg: { quality: 95 },
+            webp: { quality: 30 },
+            avif: { quality: 90 },
+        }),
+    
+  ],
 })
