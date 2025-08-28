e8aaf1df61c3655bb2f89f8e527cedb84dd5e24a
Axel Velasquez <yoloax11@gmail.com>
2025-08-28 15:14:10 -0700

    added the vercel analytics to the project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { FormspreeProvider } from '@formspree/react'
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FormspreeProvider project="2769235073248001641">
       <App />
     </FormspreeProvider>
+    <Analytics />
   </StrictMode>,
 )
