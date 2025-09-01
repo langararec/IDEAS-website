d5dcc43d4004719b72ecc5c234d4f65f76c7807d
Axel Velasquez <yoloax11@gmail.com>
2025-08-31 17:20:46 -0700

    made the gallery card mobile compatible

--- frontend/src/components/Navbar/GalleryCard.tsx
+++ frontend/src/components/Navbar/GalleryCard.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 type AboutUsCardProps = {
-    setCurrentNav: (value: string | null) => void;
+    setCurrentNav?: (value: string | null) => void;
 }
 
 const AboutUsCard: React.FC<AboutUsCardProps> = ({ setCurrentNav }) => {
     return (
-        <div className="lg:w-1/2 xl:w-1/3 mx-auto bg-white p-6 rounded-lg justify-between  shadow-md flex flex-col " onMouseLeave={() => setCurrentNav(null)}>
+        <div className=" xl:w-1/3 mx-auto bg-white xl:p-6 rounded-lg justify-between  shadow-md flex flex-col " onMouseLeave={() => setCurrentNav && setCurrentNav(null)}>
             <div>
                 {/* Header Section */}
                 <div className="bg-white  py-2">
@@ -16,19 +16,19 @@ const AboutUsCard: React.FC<AboutUsCardProps> = ({ setCurrentNav }) => {
                     </p>
                 </div>
 
-                <div className="xl:h-60 m-auto p-0">
+                <div className="xl:h-60 max-h-60 m-auto p-0 ">
                     <img
                         src="/burnabyMetrotown.png"
                         alt="Community engagement - Team "
-                        className="w-full h-48 md:h-full object-cover rounded-lg"
+                        className="w-full h-48 max-h-60 md:h-full object-cover rounded-lg"
                     />
                 </div>
 
                 {/* Content Section */}
                 <div className="flex flex-col md:flex-row">
                     {/* Left Content */}
-                    <div className="flex-1 px-6 pt-3 space-y-4 ">
-                        <Link to="/gallery" className="block hover:bg-base-100 rounded-lg">
+                    <div className="flex-1  pt-3 space-y-2 ">
+                        <Link to="/gallery" className="block hover:bg-base-200 rounded-lg p-1">
                             <div className="text-gray-700 text-lg font-medium cursor-pointer font-dm-sans">
                                 Browse Photos
                             </div>
