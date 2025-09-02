2ecfb4ba892caa7969b4a9fdb377939b522684d8
Axel Velasquez <yoloax11@gmail.com>
2025-09-02 10:24:11 -0700

    updated teamcard positionings in the container

--- frontend/src/pages/Team/TeamContainer.tsx
+++ frontend/src/pages/Team/TeamContainer.tsx
@@ -3,7 +3,7 @@ import { TeamCard } from "./TeamCard";
 export const TeamContainer = ({ members }: { members: MemberInfo[] }) => {
 
     return (
-        <div className="flex flex-col sm:flex-row sm:justify-around lg:justify-between sm:flex-wrap gap-x-8 gap-y-8">
+        <div className="flex flex-col sm:flex-row justify-center gap-8 sm:flex-wrap ">
             {members.map((member, index) => (
                 <div className="xl:w-1/5 lg:w-1/4 sm:w-5/12 w-full ">
                     <TeamCard key={index} {...member} />
