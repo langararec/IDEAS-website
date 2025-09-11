1eb2133cf6cf58fdc8a27415781c2f15a32d2c62
Axel Velasquez <yoloax11@gmail.com>
2025-09-10 21:12:47 -0700

    added the language provider to the layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { FormspreeProvider } from '@formspree/react'
+import { LanguageProvider } from './context/LanguageContext.tsx'
 import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FormspreeProvider project="2769235073248001641">
-      <App />
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
     </FormspreeProvider>
     <Analytics />
   </StrictMode>,
