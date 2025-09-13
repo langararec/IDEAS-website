a2d2ccad9b961e91d0c7a6f210cf0c765805934d
Axel Velasquez <yoloax11@gmail.com>
2025-09-12 17:46:41 -0700

    modified bentogrid to fir more use cases

--- frontend/src/pages/Gallery/BentoGrid.tsx
+++ frontend/src/pages/Gallery/BentoGrid.tsx
@@ -5,8 +5,8 @@ interface BentoGridProps {
         src: string;
         alt: string;
         title?: string;
-        colSpan: number; // Required column span (1 or 2)
-        rowSpan: number; // Required row span (1 or 2)
+        colSpan: number;
+        rowSpan: number;
     }[],
     currentPage: number;
     setCurrentPage: (page: number) => void;
@@ -14,13 +14,16 @@ interface BentoGridProps {
         previous: string;
         next: string;
     };
+    imagesPerPage?: number;
+    showPagination?: boolean;
+    layoutChange?: boolean;
+    rowHeight?: string;
 }
 
-const BentoGrid: React.FC<BentoGridProps> = ({ images, currentPage, setCurrentPage, paginationLabels }) => {
+const BentoGrid: React.FC<BentoGridProps> = ({ images, currentPage, setCurrentPage, paginationLabels, imagesPerPage = 6, showPagination = true, layoutChange = true, rowHeight = "20rem" }) => {
     const [modalOpen, setModalOpen] = useState(false);
     const [currentImage, setCurrentImage] = useState<number>(0);
     const [loadingImages, setLoadingImages] = useState<Set<number>>(new Set());
-    const imagesPerPage = 6;
 
     // Calculate pagination
     const totalPages = Math.ceil(images.length / imagesPerPage);
@@ -61,9 +64,15 @@ const BentoGrid: React.FC<BentoGridProps> = ({ images, currentPage, setCurrentPa
 
     // Function to get the correct Tailwind classes
     const getGridClasses = (colSpan: number, rowSpan: number) => {
-        const colClass = 'lg:col-span-' + colSpan;
-        const rowClass = 'lg:row-span-' + rowSpan;
-        return (colClass + ' ' + rowClass);
+        const colClass = 'col-span-' + colSpan;
+        const rowClass = 'row-span-' + rowSpan;
+
+        if (layoutChange) {
+            return ("lg:" + colClass + ' ' + "lg:" + rowClass);
+        } else {
+            return (colClass + ' ' + rowClass);
+        }
+
     };
 
     const handleImageLoad = (index: number) => {
@@ -114,7 +123,7 @@ const BentoGrid: React.FC<BentoGridProps> = ({ images, currentPage, setCurrentPa
     return (
         <div className="w-full bg-base-100" onClick={() => modalOpen ? setModalOpen(false) : null}>
 
-            <div className="hidden lg:col-span-1 lg:col-span-2 lg:col-span-3 lg:col-span-4 lg:col-span-5 lg:col-span-6 lg:row-span-1 lg:row-span-2 lg:row-span-3 lg:row-span-4 lg:row-span-5 lg:row-span-6"></div>
+            <div className="hidden lg:col-span-1 lg:col-span-2 lg:col-span-3 lg:col-span-4 lg:col-span-5 lg:col-span-6 lg:row-span-1 lg:row-span-2 lg:row-span-3 lg:row-span-4 lg:row-span-5 lg:row-span-6 col-span-1 col-span-2 col-span-3 col-span-4 row-span-1 row-span-2"></div>
 
             {modalOpen &&
                 <div className={` h-full fixed inset-0 bg-black/90 z-50 items-center justify-center flex flex-row gap-x-4`}>
@@ -125,7 +134,7 @@ const BentoGrid: React.FC<BentoGridProps> = ({ images, currentPage, setCurrentPa
                     <FaCaretRight className='text-white hover:cursor-pointer size-8 lg:size-12' onClick={(e) => { e.stopPropagation(); goToNextImage(); }} />
                 </div>}
 
-            <div className="max-w-7xl w-full grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 auto-rows-[20rem] mb-8">
+            <div className={`max-w-7xl w-full grid ${layoutChange ? 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 ' : 'grid-cols-4'} auto-rows-[${rowHeight}] gap-4 mb-8`}>
                 {currentImages.map((image, index) => (
                     <div
                         key={`galleryImage-${currentPage}-${index}`}
@@ -152,7 +161,7 @@ const BentoGrid: React.FC<BentoGridProps> = ({ images, currentPage, setCurrentPa
             </div>
 
             {/* Pagination Controls */}
-            {totalPages > 1 && (
+            {showPagination && totalPages > 1 && (
                 <div className="flex justify-center items-center gap-2 mt-8">
                     <button
                         onClick={goToPrevPage}
