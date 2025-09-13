8c2cfaab08031200e58d2a9b3f79b8aed96dd6ae
Axel Velasquez <yoloax11@gmail.com>
2025-09-13 14:18:31 -0700

    modified the mission to look good across devices

--- frontend/src/pages/Home/Mission.tsx
+++ frontend/src/pages/Home/Mission.tsx
@@ -10,13 +10,13 @@ const Mission: React.FC = () => {
         <div className="w-full mx-auto relative lg:h-96 overflow-hidden" key={`mission-${language}`}>
             {/* Desktop background image */}
             <img
-                src="/our-mission.webp"
+                src="/our-mission-cropped.webp"
                 alt={content.imageAlt}
-                className="hidden lg:block absolute -top-2/3 w-full"
+                className="hidden lg:block absolute 3xl:-top-11/12 lg:-top-1/3 xl:-top-2/3 w-full"
             />
             
             <div className="mx-auto relative h-full">
-                <div className="flex flex-col lg:justify-center lg:items-center space-y-8 lg:space-y-0 lg:h-full z-40">
+                <div className="flex flex-col lg:justify-start lg:items-center space-y-8 lg:space-y-0 lg:h-full z-40 w-full">
                     {/* Mobile image */}
                     <div className="lg:hidden pt-8  px-4">
                         <img
@@ -26,8 +26,8 @@ const Mission: React.FC = () => {
                         />
                     </div>
                     
-                    <div className="lg:flex-1 lg:flex lg:items-center lg:justify-center px-4 lg:px-8">
-                        <div className="text-center space-y-6 z-10 max-w-2xl  lg:backdrop-blur-sm lg:bg-white/10  rounded-2xl p-4 ">
+                    <div className="max-w-7xl 3xl:max-w-3/4 w-full h-full lg:flex lg:items-center lg:justify-start px-4 lg:px-8">
+                        <div className="text-center space-y-6 z-10 max-w-xl  rounded-2xl p-4 my-auto">
                             <h2 className="text-3xl lg:text-4xl font-bold text-primary font-dm-sans">
                                 {content.title}
                             </h2>
