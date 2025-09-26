0fed0dcd306e3c43ba44228f18c4121f24b4a4f0
Axel Velasquez <yoloax11@gmail.com>
2025-09-26 16:32:53 -0700

    created footer content

---
+++ frontend/src/content/FooterContent.ts
@@ -0,0 +1,14 @@
+export const footerContent = {
+  en: {
+    resources: "Resources",
+    references: "References",
+    toolkits: "Toolkits",
+    copyright: "© Innovate Recreation. All rights reserved."
+  },
+  fr: {
+    resources: "Ressources",
+    references: "Références", 
+    toolkits: "Boîtes à outils",
+    copyright: "© Innovate Recreation. Tous droits réservés."
+  }
+};
