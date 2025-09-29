b3934da42fd3a93f8162be04adc1a17bd81127b0
Axel Velasquez <yoloax11@gmail.com>
2025-09-28 17:29:56 -0700

    created the references content

---
+++ frontend/src/content/References.ts
@@ -0,0 +1,76 @@
+export const referencesContent = {
+    en: {
+        title: "References",
+        references: [
+            {
+                citing: "Agans, J. P., Powers, S. L., McCabe, M. K., & Son, J. S. (2024). \"We Try [to] Take the Barriers Away For You to Come in\": Active Recreation Contexts as Leisure Facilitators. Journal of Park & Recreation Administration, 42(1), 58-74. https://doi.org/10.18666/JPRA-2024-12122",
+                url: "https://js.sagamorepub.com/index.php/jpra/article/view/12122"
+            },
+            {
+                citing: "Beltran, A.M. (2022) Beyond Equity, Diversity and Inclusion Towards Belonging, Dignity, Justice and Joy. Royal Roads University",
+                url: "https://www.royalroads.ca/about/our-vision/equity-diversity-inclusion/equity-diversity-inclusion-rru"
+            },
+            {
+                citing: "Campbell, G., Glover, T. D., & Laryea, E. (2016). Recreation, Settlement, and the Welcoming Community: Mapping Community with African-Canadian Youth Newcomers. Leisure Sciences, 38(3), 215–231.",
+                url: "https://www.tandfonline.com/doi/full/10.1080/01490400.2015.1087896"
+            },
+            {
+                citing: "Charles-Rodriguez, U., & Larouche, R. (n.d.). Being \"outdoors\" in a new country: Associations between immigrant characteristics, outdoor recreation activities, and settlement satisfaction in Canada. Leisure Studies, 0(0), 1–15.",
+                url: "https://www.tandfonline.com/doi/full/10.1080/02614367.2024.2328087"
+            },
+            {
+                citing: "CPRA. (2016). Framework for Recreation in Canada. Canadian Parks and Recreation Association.",
+                url: "https://www.cpra.ca/framework"
+            },
+            {
+                citing: "Davis, A. M. (2021). Dignity Is the Bedrock for Workplace Belonging (SSIR).",
+                url: "https://ssir.org/articles/entry/dignity_is_the_bedrock_for_workplace_belonging"
+            },
+            {
+                citing: "Dong, C., & Morehouse, J. (2022). Toward a caring government: Advancing ethical government public relations with a care-based relationship cultivation model. Journal of Public Relations Research, 34(5), 179–207.",
+                url: "https://www.tandfonline.com/doi/full/10.1080/1062726X.2022.2119978"
+            },
+            {
+                citing: "Johnson, M. R. D. (2000). Perceptions of Barriers to Healthy Physical Activity among Asian Communities. Sport, Education & Society, 5(1), 51–70.",
+                url: "https://www.tandfonline.com/doi/abs/10.1080/135733200114433"
+            }
+        ]
+    },
+    fr: {
+        title: "Références",
+        references: [
+            {
+                citing: "Agans, J. P., Powers, S. L., McCabe, M. K., & Son, J. S. (2024). \"We Try [to] Take the Barriers Away For You to Come in\": Active Recreation Contexts as Leisure Facilitators. Journal of Park & Recreation Administration, 42(1), 58-74. https://doi.org/10.18666/JPRA-2024-12122",
+                url: "https://js.sagamorepub.com/index.php/jpra/article/view/12122"
+            },
+            {
+                citing: "Beltran, A.M. (2022) Beyond Equity, Diversity and Inclusion Towards Belonging, Dignity, Justice and Joy. Royal Roads University",
+                url: "https://www.royalroads.ca/about/our-vision/equity-diversity-inclusion/equity-diversity-inclusion-rru"
+            },
+            {
+                citing: "Campbell, G., Glover, T. D., & Laryea, E. (2016). Recreation, Settlement, and the Welcoming Community: Mapping Community with African-Canadian Youth Newcomers. Leisure Sciences, 38(3), 215–231.",
+                url: "https://www.tandfonline.com/doi/full/10.1080/01490400.2015.1087896"
+            },
+            {
+                citing: "Charles-Rodriguez, U., & Larouche, R. (n.d.). Being \"outdoors\" in a new country: Associations between immigrant characteristics, outdoor recreation activities, and settlement satisfaction in Canada. Leisure Studies, 0(0), 1–15.",
+                url: "https://www.tandfonline.com/doi/full/10.1080/02614367.2024.2328087"
+            },
+            {
+                citing: "CPRA. (2016). Framework for Recreation in Canada. Canadian Parks and Recreation Association.",
+                url: "https://www.cpra.ca/framework"
+            },
+            {
+                citing: "Davis, A. M. (2021). Dignity Is the Bedrock for Workplace Belonging (SSIR).",
+                url: "https://ssir.org/articles/entry/dignity_is_the_bedrock_for_workplace_belonging"
+            },
+            {
+                citing: "Dong, C., & Morehouse, J. (2022). Toward a caring government: Advancing ethical government public relations with a care-based relationship cultivation model. Journal of Public Relations Research, 34(5), 179–207.",
+                url: "https://www.tandfonline.com/doi/full/10.1080/1062726X.2022.2119978"
+            },
+            {
+                citing: "Johnson, M. R. D. (2000). Perceptions of Barriers to Healthy Physical Activity among Asian Communities. Sport, Education & Society, 5(1), 51–70.",
+                url: "https://www.tandfonline.com/doi/abs/10.1080/135733200114433"
+            }
+        ]
+    }
+}
