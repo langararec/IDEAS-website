3c3a84e081d64ef7765d3091aebc3ce63ccc3856
Axel Velasquez <yoloax11@gmail.com>
2025-10-23 16:06:03 -0700

    removed the uppercase class

--- frontend/src/pages/Home/Hero.tsx
+++ frontend/src/pages/Home/Hero.tsx
@@ -27,7 +27,7 @@ const Hero: React.FC = () => {
                         target="_blank" 
                         rel="noopener noreferrer" 
                         aria-label={content.surveyAriaLabel}
-                        className="inline-block bg-primary hover:bg-primary/80 text-white border-3 px-8 py-3 rounded-xl mt-4  focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 transition-all font-medium text-center duration-300  hover:scale-105"
+                        className="inline-block bg-primary font-dm-sans hover:bg-primary/80 text-white border-3 px-8 py-3 rounded-xl mt-4  focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 transition-all font-medium text-center duration-300  hover:scale-105"
                     >
                         {content.surveyButton}
                     </a>
