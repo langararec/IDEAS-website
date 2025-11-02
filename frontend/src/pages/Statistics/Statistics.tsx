005de8da78e896b704528836cd2e236e29d24a17
Axel Velasquez <yoloax11@gmail.com>
2025-11-01 17:36:51 -0700

    add Statistics component with toggle and demographic sections

---
+++ frontend/src/pages/Statistics/Statistics.tsx
@@ -0,0 +1,16 @@
+import StatisticsToggle from "./StatisticsToggle";
+import Demographics from "./Demographics";
+import RecreationBehaviors from "./RecreationBehaviors";
+
+const Statistics = () => {
+
+    return (
+        <div>
+            <StatisticsToggle />
+            <Demographics />
+            <RecreationBehaviors />
+        </div>
+    );
+};
+
+export default Statistics;
