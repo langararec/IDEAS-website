445b561885780433eb90e5d9106bce423cec5aa5
Axel Velasquez <yoloax11@gmail.com>
2025-11-04 16:49:43 -0800

    refactor: remove unused components from Statistics page for cleaner code

--- frontend/src/pages/Statistics/Statistics.tsx
+++ frontend/src/pages/Statistics/Statistics.tsx
@@ -1,17 +1,10 @@
 import StatisticsToggle from "./StatisticsToggle";
-import Demographics from "./Demographics";
-import RecreationBehaviors from "./RecreationBehaviors";
-import ThoughtsOnRecreation from "./ThoughtsOnRecreation";
-import CommunicationChannels from "./CommunicationChannels";
+
 const Statistics = () => {
 
     return (
         <div>
             <StatisticsToggle />
-            <Demographics />
-            <RecreationBehaviors />
-            <ThoughtsOnRecreation />
-            <CommunicationChannels />
         </div>
     );
 };
