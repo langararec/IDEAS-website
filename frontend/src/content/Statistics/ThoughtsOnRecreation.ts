6109fe8a5d6cf1b8e070399f0cc13c2b0b1db9e2
Axel Velasquez <yoloax11@gmail.com>
2025-12-19 14:22:42 -0600

    feat: add bilingual content for Thoughts on Recreation statistics

---
+++ frontend/src/content/Statistics/ThoughtsOnRecreation.ts
@@ -0,0 +1,366 @@
+export const en = {
+  title: "Thoughts on Recreation",
+  subtitle: "Community perspective on recreation access and inclusivity",
+  scaleLabel: "1 = Strongly Disagree, 3 = Neutral, 5 = Strongly Agree",
+  burnaby: {
+    lastUpdated: "Last updated: 30-11-2025",
+    keyInsightsLabel: "Key Insights:",
+    keyInsights:
+      "Survey responses indicate that there are differences in how strongly they agreed with each statement presented to them. More work needs to be done to understand the mechanisms behind these differences.",
+    lowestAverage: {
+      charts: [
+        {
+          question: "There are enough recreation activities in my community",
+          average: 3.1,
+          ethnicities: [
+            { name: "Filipino", score: 1.5 },
+            { name: "Southeast Asian", score: 2.0 },
+            { name: "Japanese", score: 2.5 },
+            { name: "Black", score: 2.7 },
+            { name: "West Asian", score: 3.2 },
+            { name: "Korean", score: 3.3 },
+            { name: "South Asian", score: 3.5 },
+            { name: "Chinese", score: 3.6 },
+            { name: "White", score: 3.6 },
+            { name: "Arab", score: 3.7 },
+            { name: "Indigenous People", score: 4.0 },
+            { name: "Latin American", score: 4.0 },
+          ],
+        },
+        {
+          question:
+            "There are enough recreation activities for people of all ages in my community",
+          average: 3.3,
+          ethnicities: [
+            { name: "Filipino", score: 2.5 },
+            { name: "Japanese", score: 2.5 },
+            { name: "Southeast Asian", score: 3.0 },
+            { name: "West Asian", score: 3.0 },
+            { name: "Black", score: 3.3 },
+            { name: "Korean", score: 3.3 },
+            { name: "Chinese", score: 3.5 },
+            { name: "Arab", score: 3.5 },
+            { name: "Latin American", score: 3.5 },
+            { name: "White", score: 3.7 },
+            { name: "Indigenous People", score: 4.0 },
+            { name: "South Asian", score: 4.0 },
+          ],
+        },
+      ],
+    },
+    highestAverage: {
+      charts: [
+        {
+          question: "I feel welcome in places like parks and community centers",
+          average: 4.3,
+          ethnicities: [
+            { name: "Japanese", score: 3.0 },
+            { name: "Arab", score: 3.8 },
+            { name: "Indigenous People", score: 4.0 },
+            { name: "West Asian", score: 4.0 },
+            { name: "South Asian", score: 4.2 },
+            { name: "Black", score: 4.3 },
+            { name: "Korean", score: 4.3 },
+            { name: "Chinese", score: 4.4 },
+            { name: "Filipino", score: 4.5 },
+            { name: "White", score: 4.6 },
+            { name: "Latin American", score: 4.8 },
+            { name: "Southeast Asian", score: 5.0 },
+          ],
+        },
+        {
+          question:
+            "I have fun when I do recreation activities in my community",
+          average: 4.3,
+          ethnicities: [
+            { name: "Japanese", score: 3.0 },
+            { name: "Indigenous People", score: 4.0 },
+            { name: "Korean", score: 4.0 },
+            { name: "South Asian", score: 4.0 },
+            { name: "Arab", score: 4.0 },
+            { name: "West Asian", score: 4.2 },
+            { name: "Black", score: 4.3 },
+            { name: "Chinese", score: 4.5 },
+            { name: "White", score: 4.5 },
+            { name: "Filipino", score: 4.5 },
+            { name: "Latin American", score: 5.0 },
+            { name: "Southeast Asian", score: 5.0 },
+          ],
+        },
+      ],
+    },
+  },
+  courtenay: {
+    lastUpdated: "Last updated: 30-11-2025",
+    keyInsightsLabel: "Key Insights:",
+    keyInsights:
+      "Survey responses indicate that there are differences in how strongly they agreed with each statement presented to them. More work needs to be done to understand the mechanisms behind these differences.",
+    lowestAverage: {
+      charts: [
+        {
+          question: "There are enough recreation activities in my community",
+          average: 3.2,
+          ethnicities: [
+            { name: "Black", score: 1.5 },
+            { name: "Indigenous People", score: 2.0 },
+            { name: "Chinese", score: 3.0 },
+            { name: "Korean", score: 3.0 },
+            { name: "South Asian", score: 3.0 },
+            { name: "White", score: 3.4 },
+            { name: "Filipino", score: 3.5 },
+            { name: "Japanese", score: 3.5 },
+            { name: "Southeast Asian", score: 3.5 },
+            { name: "Latin American", score: 4.0 },
+            { name: "West Asian", score: 5.0 },
+          ],
+        },
+        {
+          question:
+            "There are enough recreation activities for people of all ages in my community",
+          average: 3.3,
+          ethnicities: [
+            { name: "Korean", score: 2.0 },
+            { name: "Southeast Asian", score: 2.5 },
+            { name: "South Asian", score: 2.8 },
+            { name: "Chinese", score: 3.0 },
+            { name: "Black", score: 3.5 },
+            { name: "Filipino", score: 3.5 },
+            { name: "White", score: 3.5 },
+            { name: "Japanese", score: 4.0 },
+            { name: "Latin American", score: 4.0 },
+            { name: "West Asian", score: 4.0 },
+            { name: "Prefer not to disclose", score: 4.5 },
+          ],
+        },
+      ],
+    },
+    highestAverage: {
+      charts: [
+        {
+          question: "I feel welcome in places like parks and community centers",
+          average: 4.3,
+          ethnicities: [
+            { name: "Korean", score: 3.0 },
+            { name: "South Asian", score: 3.4 },
+            { name: "Indigenous People", score: 3.5 },
+            { name: "Filipino", score: 4.0 },
+            { name: "Chinese", score: 4.3 },
+            { name: "White", score: 4.5 },
+            { name: "Japanese", score: 4.5 },
+            { name: "Southeast Asian", score: 4.5 },
+            { name: "Latin American", score: 4.7 },
+            { name: "Arab", score: 5.0 },
+            { name: "Black", score: 5.0 },
+            { name: "Prefer not to disclose", score: 5.0 },
+            { name: "West Asian", score: 5.0 },
+          ],
+        },
+        {
+          question:
+            "I have fun when I do recreation activities in my community",
+          average: 4.3,
+          ethnicities: [
+            { name: "Korean", score: 2.0 },
+            { name: "South Asian", score: 3.4 },
+            { name: "Chinese", score: 3.7 },
+            { name: "Black", score: 4.0 },
+            { name: "Filipino", score: 4.0 },
+            { name: "West Asian", score: 4.0 },
+            { name: "White", score: 4.3 },
+            { name: "Indigenous People", score: 4.5 },
+            { name: "Prefer not to disclose", score: 4.5 },
+            { name: "Southeast Asian", score: 4.5 },
+            { name: "Latin American", score: 4.7 },
+            { name: "Japanese", score: 5.0 },
+          ],
+        },
+      ],
+    },
+  },
+};
+
+export const fr = {
+  title: "Pensées sur les Loisirs",
+  subtitle:
+    "Perspective communautaire sur l'accès et l'inclusivité des loisirs",
+  scaleLabel: "1 = Fortement en désaccord, 3 = Neutre, 5 = Fortement d'accord",
+  burnaby: {
+    lastUpdated: "Dernière mise à jour: 30-11-2025",
+    keyInsightsLabel: "Aperçus Clés:",
+    keyInsights:
+      "Les réponses au sondage indiquent qu'il existe des différences dans la force avec laquelle les participants ont approuvé chaque énoncé qui leur a été présenté. Davantage de travail est nécessaire pour comprendre les mécanismes derrière ces différences.",
+    lowestAverage: {
+      charts: [
+        {
+          question:
+            "Il y a suffisamment d'activités récréatives dans ma communauté",
+          average: 3.1,
+          ethnicities: [
+            { name: "Philippin", score: 1.5 },
+            { name: "Asiatique du Sud-Est", score: 2.0 },
+            { name: "Japonais", score: 2.5 },
+            { name: "Noir", score: 2.7 },
+            { name: "Asie de l'Ouest", score: 3.2 },
+            { name: "Coréen", score: 3.3 },
+            { name: "Sud-Asiatique", score: 3.5 },
+            { name: "Chinois", score: 3.6 },
+            { name: "Blanc", score: 3.6 },
+            { name: "Arabe", score: 3.7 },
+            { name: "Peuples autochtones", score: 4.0 },
+            { name: "Latino-Américain", score: 4.0 },
+          ],
+        },
+        {
+          question:
+            "Il y a suffisamment d'activités récréatives pour les personnes de tous âges dans ma communauté",
+          average: 3.3,
+          ethnicities: [
+            { name: "Philippin", score: 2.5 },
+            { name: "Japonais", score: 2.5 },
+            { name: "Asiatique du Sud-Est", score: 3.0 },
+            { name: "Asie de l'Ouest", score: 3.0 },
+            { name: "Noir", score: 3.3 },
+            { name: "Coréen", score: 3.3 },
+            { name: "Chinois", score: 3.5 },
+            { name: "Arabe", score: 3.5 },
+            { name: "Latino-Américain", score: 3.5 },
+            { name: "Blanc", score: 3.7 },
+            { name: "Peuples autochtones", score: 4.0 },
+            { name: "Sud-Asiatique", score: 4.0 },
+          ],
+        },
+      ],
+    },
+    highestAverage: {
+      charts: [
+        {
+          question:
+            "Je me sens bienvenu dans des endroits comme les parcs et centres communautaires",
+          average: 4.3,
+          ethnicities: [
+            { name: "Japonais", score: 3.0 },
+            { name: "Arabe", score: 3.8 },
+            { name: "Peuples autochtones", score: 4.0 },
+            { name: "Asie de l'Ouest", score: 4.0 },
+            { name: "Sud-Asiatique", score: 4.2 },
+            { name: "Noir", score: 4.3 },
+            { name: "Coréen", score: 4.3 },
+            { name: "Chinois", score: 4.4 },
+            { name: "Philippin", score: 4.5 },
+            { name: "Blanc", score: 4.6 },
+            { name: "Latino-Américain", score: 4.8 },
+            { name: "Asiatique du Sud-Est", score: 5.0 },
+          ],
+        },
+        {
+          question:
+            "Je m'amuse quand je fais des activités récréatives dans ma communauté",
+          average: 4.3,
+          ethnicities: [
+            { name: "Japonais", score: 3.0 },
+            { name: "Peuples autochtones", score: 4.0 },
+            { name: "Coréen", score: 4.0 },
+            { name: "Sud-Asiatique", score: 4.0 },
+            { name: "Arabe", score: 4.0 },
+            { name: "Asie de l'Ouest", score: 4.2 },
+            { name: "Noir", score: 4.3 },
+            { name: "Chinois", score: 4.5 },
+            { name: "Blanc", score: 4.5 },
+            { name: "Philippin", score: 4.5 },
+            { name: "Latino-Américain", score: 5.0 },
+            { name: "Asiatique du Sud-Est", score: 5.0 },
+          ],
+        },
+      ],
+    },
+  },
+  courtenay: {
+    lastUpdated: "Dernière mise à jour: 30-11-2025",
+    keyInsightsLabel: "Aperçus Clés:",
+    keyInsights:
+      "Les réponses au sondage indiquent qu'il existe des différences dans la force avec laquelle les participants ont approuvé chaque énoncé qui leur a été présenté. Davantage de travail est nécessaire pour comprendre les mécanismes derrière ces différences.",
+    lowestAverage: {
+      charts: [
+        {
+          question:
+            "Il y a suffisamment d'activités récréatives dans ma communauté",
+          average: 3.2,
+          ethnicities: [
+            { name: "Noir", score: 1.5 },
+            { name: "Peuples autochtones", score: 2.0 },
+            { name: "Chinois", score: 3.0 },
+            { name: "Coréen", score: 3.0 },
+            { name: "Sud-Asiatique", score: 3.0 },
+            { name: "Blanc", score: 3.4 },
+            { name: "Philippin", score: 3.5 },
+            { name: "Japonais", score: 3.5 },
+            { name: "Asiatique du Sud-Est", score: 3.5 },
+            { name: "Latino-Américain", score: 4.0 },
+            { name: "Asie de l'Ouest", score: 5.0 },
+          ],
+        },
+        {
+          question:
+            "Il y a suffisamment d'activités récréatives pour les personnes de tous âges dans ma communauté",
+          average: 3.3,
+          ethnicities: [
+            { name: "Coréen", score: 2.0 },
+            { name: "Asiatique du Sud-Est", score: 2.5 },
+            { name: "Sud-Asiatique", score: 2.8 },
+            { name: "Chinois", score: 3.0 },
+            { name: "Noir", score: 3.5 },
+            { name: "Philippin", score: 3.5 },
+            { name: "Blanc", score: 3.5 },
+            { name: "Japonais", score: 4.0 },
+            { name: "Latino-Américain", score: 4.0 },
+            { name: "Asie de l'Ouest", score: 4.0 },
+            { name: "Préfère ne pas divulguer", score: 4.5 },
+          ],
+        },
+      ],
+    },
+    highestAverage: {
+      charts: [
+        {
+          question:
+            "Je me sens bienvenu dans des endroits comme les parcs et centres communautaires",
+          average: 4.3,
+          ethnicities: [
+            { name: "Coréen", score: 3.0 },
+            { name: "Sud-Asiatique", score: 3.4 },
+            { name: "Peuples autochtones", score: 3.5 },
+            { name: "Philippin", score: 4.0 },
+            { name: "Chinois", score: 4.3 },
+            { name: "Blanc", score: 4.5 },
+            { name: "Japonais", score: 4.5 },
+            { name: "Asiatique du Sud-Est", score: 4.5 },
+            { name: "Latino-Américain", score: 4.7 },
+            { name: "Arabe", score: 5.0 },
+            { name: "Noir", score: 5.0 },
+            { name: "Préfère ne pas divulguer", score: 5.0 },
+            { name: "Asie de l'Ouest", score: 5.0 },
+          ],
+        },
+        {
+          question:
+            "Je m'amuse quand je fais des activités récréatives dans ma communauté",
+          average: 4.3,
+          ethnicities: [
+            { name: "Coréen", score: 2.0 },
+            { name: "Sud-Asiatique", score: 3.4 },
+            { name: "Chinois", score: 3.7 },
+            { name: "Noir", score: 4.0 },
+            { name: "Philippin", score: 4.0 },
+            { name: "Asie de l'Ouest", score: 4.0 },
+            { name: "Blanc", score: 4.3 },
+            { name: "Peuples autochtones", score: 4.5 },
+            { name: "Préfère ne pas divulguer", score: 4.5 },
+            { name: "Asiatique du Sud-Est", score: 4.5 },
+            { name: "Latino-Américain", score: 4.7 },
+            { name: "Japonais", score: 5.0 },
+          ],
+        },
+      ],
+    },
+  },
+};
