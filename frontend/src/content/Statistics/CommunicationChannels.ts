e87c98e747879594705ad45d620aa6ee84d213e4
Axel Velasquez <yoloax11@gmail.com>
2025-12-19 14:22:27 -0600

    feat: add bilingual content for communication channels in Statistics

---
+++ frontend/src/content/Statistics/CommunicationChannels.ts
@@ -0,0 +1,86 @@
+export const en = {
+  title: "Where people find out recreation activities",
+  subtitle:
+    "How length of residency in BC influences communication channel preferences",
+  chartLabel: "Percentage (%)",
+  residencyGroups: [
+    "Less than 1 year",
+    "1-3 years",
+    "4-6 years",
+    "More than 6 years",
+  ],
+  channels: [
+    "Community/Cultural Groups",
+    "Friends/Family",
+    "Newspaper/Radio",
+    "Posters/Flyers/Signs",
+    "Recreation Centre/Website",
+    "School/Workplace",
+    "Social Media",
+  ],
+  burnaby: {
+    lastUpdated: "Last updated: 11-11-2025",
+    keyInsightsLabel: "Key Insights:",
+    keyInsights:
+      "New residents rely more on social media, friends and community groups. While residents with longer time living in BC are more reliant through posters, schools, newspapers and city websites.",
+    data: {
+      "Less than 1 year": [15, 11, 0, 11, 16, 12, 9],
+      "1-3 years": [6, 9, 8, 7, 8, 6, 15],
+      "4-6 years": [30, 17, 15, 11, 8, 18, 30],
+      "More than 6 years": [48, 63, 77, 71, 68, 65, 47],
+    },
+  },
+  courtenay: {
+    lastUpdated: "Last updated: 11-11-2025",
+    keyInsightsLabel: "Key Insights:",
+    keyInsights:
+      "New residents rely more on community or cultural groups, schools/workplaces and social media. Long-term residents are the most users of all sources, especially city websites, local newspapers and posters/flyers.",
+    data: {
+      "Less than 1 year": [4, 5, 0, 3, 2, 8, 9],
+      "1-3 years": [46, 29, 7, 19, 12, 35, 27],
+      "4-6 years": [4, 3, 14, 9, 2, 4, 2],
+      "More than 6 years": [46, 63, 79, 69, 83, 54, 61],
+    },
+  },
+};
+
+export const fr = {
+  title: "Où les gens découvrent les activités récréatives",
+  subtitle:
+    "Comment la durée de résidence en C.-B. influence les préférences des canaux de communication",
+  chartLabel: "Pourcentage (%)",
+  residencyGroups: ["Moins d'1 an", "1-3 ans", "4-6 ans", "Plus de 6 ans"],
+  channels: [
+    "Groupes Communautaires/Culturels",
+    "Amis/Famille",
+    "Journal/Radio",
+    "Affiches/Dépliants/Panneaux",
+    "Centre de Loisirs/Site Web",
+    "École/Lieu de Travail",
+    "Médias Sociaux",
+  ],
+  burnaby: {
+    lastUpdated: "Dernière mise à jour: 15-10-2025",
+    keyInsightsLabel: "Aperçus Clés:",
+    keyInsights:
+      "Les nouveaux résidents comptent davantage sur les médias sociaux, les amis et les groupes communautaires. Alors que les résidents vivant en C.-B. depuis plus longtemps s'appuient davantage sur les affiches, les écoles, les journaux et les sites Web municipaux.",
+    data: {
+      "Moins d'1 an": [15, 11, 0, 11, 16, 12, 9],
+      "1-3 ans": [6, 9, 8, 7, 8, 6, 15],
+      "4-6 ans": [30, 17, 15, 11, 8, 18, 30],
+      "Plus de 6 ans": [48, 63, 77, 71, 68, 65, 47],
+    },
+  },
+  courtenay: {
+    lastUpdated: "Dernière mise à jour: 15-10-2025",
+    keyInsightsLabel: "Aperçus Clés:",
+    keyInsights:
+      "Les nouveaux résidents comptent davantage sur les groupes communautaires ou culturels, les écoles/lieux de travail et les médias sociaux. Les résidents de longue date sont les plus grands utilisateurs de toutes les sources, en particulier les sites Web municipaux, les journaux locaux et les affiches/dépliants.",
+    data: {
+      "Moins d'1 an": [4, 5, 0, 3, 2, 8, 9],
+      "1-3 ans": [46, 29, 7, 19, 12, 35, 27],
+      "4-6 ans": [4, 3, 14, 9, 2, 4, 2],
+      "Plus de 6 ans": [46, 63, 79, 69, 83, 54, 61],
+    },
+  },
+};
