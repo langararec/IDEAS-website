6124eadec2444d6384f22d11f475eed55d760c5c
Axel Velasquez <yoloax11@gmail.com>
2025-12-19 14:22:31 -0600

    feat: add bilingual demographics content for Statistics

---
+++ frontend/src/content/Statistics/Demographics.ts
@@ -0,0 +1,147 @@
+export const en = {
+  title: "Demographics",
+  genderIdentity: {
+    title: "Gender Identity",
+    subtitle: "How respondents describe themselves",
+    burnaby: {
+      lastUpdated: "Last updated: 11-11-2025",
+      data: [
+        { name: "Woman", percentage: 76.12 },
+        { name: "Man", percentage: 22.39 },
+        { name: "Non-binary", percentage: 0.75 },
+        { name: "Transgender", percentage: 0.75 },
+      ],
+    },
+    courtenay: {
+      lastUpdated: "Last updated: 11-11-2025",
+      data: [
+        { name: "Woman", percentage: 65.26 },
+        { name: "Man", percentage: 32.63 },
+        { name: "Non-binary", percentage: 1.05 },
+        { name: "Transgender", percentage: 1.05 },
+      ],
+    },
+  },
+  age: {
+    title: "Age",
+    subtitle: "Respondents age group",
+    burnaby: {
+      lastUpdated: "Last updated: 11-11-2025",
+      data: [
+        { name: "35-44", percentage: 26.28 },
+        { name: "55 and above", percentage: 25.55 },
+        { name: "45-54", percentage: 20.44 },
+        { name: "25-34", percentage: 18.98 },
+        { name: "18-24", percentage: 5.11 },
+        { name: "Under 18", percentage: 3.65 },
+      ],
+    },
+    courtenay: {
+      lastUpdated: "Last updated: 11-11-2025",
+      data: [
+        { name: "55 and above", percentage: 27.37 },
+        { name: "35-44", percentage: 24.21 },
+        { name: "45-54", percentage: 21.05 },
+        { name: "18-24", percentage: 13.68 },
+        { name: "Under 18", percentage: 7.37 },
+        { name: "25-34", percentage: 6.32 },
+      ],
+    },
+  },
+  timeLivingInBC: {
+    title: "Time Living in BC",
+    subtitle: "Years of living in British Columbia",
+    burnaby: {
+      lastUpdated: "Last updated: 11-11-2025",
+      data: [
+        { name: "More than 6 years", percentage: 62.22 },
+        { name: "1-3 years", percentage: 14.07 },
+        { name: "4-6 years", percentage: 13.33 },
+        { name: "Less than 1 year", percentage: 10.37 },
+      ],
+    },
+    courtenay: {
+      lastUpdated: "Last updated: 11-11-2025",
+      data: [
+        { name: "More than 6 years", percentage: 58.95 },
+        { name: "1-3 years", percentage: 25.26 },
+        { name: "Less than 1 year", percentage: 10.53 },
+        { name: "4-6 years", percentage: 5.26 },
+      ],
+    },
+  },
+};
+
+export const fr = {
+  title: "Démographie",
+  genderIdentity: {
+    title: "Identité de Genre",
+    subtitle: "Comment les répondants se décrivent",
+    burnaby: {
+      lastUpdated: "Dernière mise à jour: 15-10-2025",
+      data: [
+        { name: "Femme", percentage: 76.12 },
+        { name: "Homme", percentage: 22.39 },
+        { name: "Non-binaire", percentage: 0.75 },
+        { name: "Transgenre", percentage: 0.75 },
+      ],
+    },
+    courtenay: {
+      lastUpdated: "Dernière mise à jour: 15-10-2025",
+      data: [
+        { name: "Femme", percentage: 65.26 },
+        { name: "Homme", percentage: 32.63 },
+        { name: "Non-binaire", percentage: 1.05 },
+        { name: "Transgenre", percentage: 1.05 },
+      ],
+    },
+  },
+  age: {
+    title: "Âge",
+    subtitle: "Groupe d'âge des répondants",
+    burnaby: {
+      lastUpdated: "Dernière mise à jour: 15-10-2025",
+      data: [
+        { name: "35-44", percentage: 26.28 },
+        { name: "55 et plus", percentage: 25.55 },
+        { name: "45-54", percentage: 20.44 },
+        { name: "25-34", percentage: 18.98 },
+        { name: "18-24", percentage: 5.11 },
+        { name: "Moins de 18", percentage: 3.65 },
+      ],
+    },
+    courtenay: {
+      lastUpdated: "Dernière mise à jour: 15-10-2025",
+      data: [
+        { name: "55 et plus", percentage: 27.37 },
+        { name: "35-44", percentage: 24.21 },
+        { name: "45-54", percentage: 21.05 },
+        { name: "18-24", percentage: 13.68 },
+        { name: "Moins de 18", percentage: 7.37 },
+        { name: "25-34", percentage: 6.32 },
+      ],
+    },
+  },
+  timeLivingInBC: {
+    title: "Temps Vécu en C.-B.",
+    subtitle: "Années de vie en Colombie-Britannique",
+    burnaby: {
+      lastUpdated: "Dernière mise à jour: 15-10-2025",
+      data: [
+        { name: "Plus de 6 ans", percentage: 62.22 },
+        { name: "1-3 ans", percentage: 14.07 },
+        { name: "4-6 ans", percentage: 13.33 },
+        { name: "Moins d'1 an", percentage: 10.37 },
+      ],
+    },
+    courtenay: {
+      lastUpdated: "Dernière mise à jour: 15-10-2025",
+      data: [
+        { name: "Plus de 6 ans", percentage: 58.95 },
+        { name: "1-3 ans", percentage: 25.26 },
+        { name: "Moins d'1 an", percentage: 10.53 },
+        { name: "4-6 ans", percentage: 5.26 },
+      ],
+    },
+  },
+};
